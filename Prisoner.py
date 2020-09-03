5882c698ec1a3d29fd751d83824e575ac5f1c5da
Jake Vossen <jake@vossen.dev>
2020-09-02 22:24:39 -0600

    minor updates

--- Prisoner.py
+++ Prisoner.py
@@ -1,8 +1,9 @@
-class Prisonor:
+class Prisoner:
     id = 0
     found = False
 
     def __init__(self, id):
         self.id = id
+
     def get_id():
         return self.id
--- prisprob.py
+++ prisprob.py
@@ -1,53 +1,53 @@
-from Prisonor import Prisonor
+from Prisoner import Prisoner
 from Box import Box
 import random
 
-def main():
-    # Initilizing list of prisonors
-    list_of_prisonors = []
-    for n in range(100):
-        list_of_prisonors.append(Prisonor(n))
 
-    # Initilizing list of boxes
-    list_of_boxes = []
-    random_val_array = [] # to set up what is inside boxes
+def main():
+    # Initializing list of prisonors
+    list_of_prisoners = []
     for n in range(100):
-        random_val_array.append(n)
+        list_of_prisoners.append(Prisoner(n))
+
+    # Initializing list of boxes
+    list_of_boxes = []
+    # to set up what is inside boxes
+    random_val_array = [n for n in range(100)]
     random.shuffle(random_val_array)
     # print(random_val_array)
 
-    # Initilize array of boxes
+    # Initializing array of boxes
     for n in range(100):
         list_of_boxes.append(Box(n, random_val_array[n]))
 
-    #misc inits
-    falseones = 0
-    correctones = 0
-    runningaverage = []
-    
-    for n in range(10000):# Testing
+    # misc inits
+    false_ones = 0
+    correct_ones = 0
+
+    for n in range(10000):
         temp = True
         for n in range(100):
-            if (check_if_in_loop(list_of_prisonors[n], list_of_boxes) == False):
+            if (not check_if_in_loop(list_of_prisoners[n], list_of_boxes)):
                 temp = False
         if (temp):
-            correctones = correctones + 1
+            correct_ones = correct_ones + 1
         else:
-            falseones = falseones + 1
+            false_ones = false_ones + 1
 
         random.shuffle(random_val_array)
         for curbox in list_of_boxes:
-            curbox.set_val_in_box(random_val_array[list_of_boxes.index(curbox)])
-    print(correctones / (correctones + falseones))
- 
-def check_if_in_loop(prisonor, list_of_boxes):
-    timeswent = 0
-    nextbox = list_of_boxes[prisonor.id]
+            curbox.set_val_in_box(
+                random_val_array[list_of_boxes.index(curbox)])
+    print(correct_ones / (correct_ones + false_ones))
+
+
+def check_if_in_loop(prisoner, list_of_boxes):
+    nextbox = list_of_boxes[prisoner.id]
     counter = 0
-    while (prisonor.id != nextbox.val_in_box):
+    while (prisoner.id != nextbox.val_in_box):
         counter = counter + 1
         nextbox = list_of_boxes[nextbox.val_in_box]
     return counter < 50
+
+
 main()
-
-
